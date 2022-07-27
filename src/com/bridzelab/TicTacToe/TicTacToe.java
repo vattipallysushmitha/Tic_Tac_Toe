5a927cd9fbf02ccc10d4707be3affd873bcbeb66
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-27 15:09:55 +0530

    [sushmitha] showBoard to display the current Board

--- src/com/bridzelab/TicTacToe/TicTacToe.java
+++ src/com/bridzelab/TicTacToe/TicTacToe.java
@@ -7,45 +7,53 @@ public class TicTacToe
 	public static char ComputerLetter = ' ';
 
 
-		 public static void main(String[] args)
-		 {
-			 
-			 
-		       System.out.println("************Welcome to TicTacToe on workshop platform*******************");
-		       char[] board = createBoard();
-		       char UserLetter = chooseLetter();
-		       char ComputerLetter;
-		       if(UserLetter=='X')
+	public static void main(String[] args) {
+	       System.out.println("************Welcome to TicTacToe on workshop platform*******************\"");
+	       char[] board = createBoard();
+	       char UserLetter = chooseLetter();
+	       char ComputerLetter;
+	       if(UserLetter=='X')
 
-		           ComputerLetter='O';
+	           ComputerLetter='O';
 
-		       else
+	       else
 
-		           ComputerLetter='X';
+	           ComputerLetter='X';
 
-		       System.out.println(" Now the  Computer Letter is::::::: " + ComputerLetter);
-		       System.out.println(" Now the User Letter is ::::::: " + UserLetter);
-		 }
-		 
-		 
+	       System.out.println("Computer Letter is::::::::::: " + ComputerLetter);
+	       System.out.println("User Letter is:::::::::" + UserLetter);
 
-		    public static char chooseLetter(){
-		        char symbol;
-		        Scanner UserInput = new Scanner(System.in);
-		        System.out.print("Enter the symbol to allow the player to choose the symbol");
-		        symbol=UserInput.next().toUpperCase().charAt(0);
-		        return symbol;
-		    }
+	       
+	       showBoard(board);
+	   }
+
+	    
+	    public static void showBoard(char[] board)
+	    {
+	        System.out.println("  |  " + board[1] + "  |  "  + board[2] + "  |  " + board[3]);
+	        System.out.println("--------------");
+	        System.out.println("  |  " + board[4] + "  |  "  + board[5] + "  |  " + board[6]);
+	        System.out.println("--------------");
+	        System.out.println("  |  " + board[7] + "  |  "  + board[8] + "  |  " + board[9]);
+	        System.out.println("--------------");
+	    }
 
 
-		    public static char[] createBoard() {
+	    public static char[] createBoard() {
 
-		        char[] board = new char[10];
-		        for(int index = 0; index < board.length; index++)
-		        {
-		            board[index] = ' ';
-		        }
-		        return  board;
-		    }
+	        char[] board = new char[10];
+	        for(int index = 0; index < board.length; index++)
+	        {
+	            board[index] = ' ';
+	        }
+	        return  board;
+	    }
+	    public static char chooseLetter(){
+	        char symbol;
+	        Scanner UserInput = new Scanner(System.in);
+	        System.out.print("Enter the symbol to allow the player to choose the symbol");
+	        symbol=UserInput.next().toUpperCase().charAt(0);
+	        return symbol;
+	    }
 
-		}
+	}
