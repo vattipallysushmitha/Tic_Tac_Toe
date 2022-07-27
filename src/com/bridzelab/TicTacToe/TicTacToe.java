49d0dc186a104ca42894a8db070d2d66f2de2fcf
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-27 09:27:45 +0530

    [sushmitha] added allow the user to choose the symboll

--- src/com/bridzelab/TicTacToe/TicTacToe.java
+++ src/com/bridzelab/TicTacToe/TicTacToe.java
@@ -1,24 +1,49 @@
 package com.bridzelab.TicTacToe;
-
+import java.util.Scanner;
 public class TicTacToe
 {
-	char[] board = new char[10];
-		public void Board()
-		{
-			
-			for(int i=1; i<board.length; i++)
-			{
-				board[i]= '';
-				
-			}
+	
+	
+
+		 public static void main(String[] args)
+		 {
+			 
+			 
+		       System.out.println("************Welcome to TicTacToe on workshop platform*******************");
+		       char[] board = createBoard();
+		       char UserLetter = chooseLetter();
+		       char ComputerLetter;
+		       if(UserLetter=='X')
+
+		           ComputerLetter='O';
+
+		       else
+
+		           ComputerLetter='X';
+
+		       System.out.println(" Now the  Computer Letter is::::::: " + ComputerLetter);
+		       System.out.println(" Now the User Letter is ::::::: " + UserLetter);
+		 }
+		 
+		 
+
+		    public static char chooseLetter(){
+		        char symbol;
+		        Scanner UserInput = new Scanner(System.in);
+		        System.out.print("Enter the symbol to allow the player to choose the symbol");
+		        symbol=UserInput.next().toUpperCase().charAt(0);
+		        return symbol;
+		    }
+
+
+		    public static char[] createBoard() {
+
+		        char[] board = new char[10];
+		        for(int index = 0; index < board.length; index++)
+		        {
+		            board[index] = ' ';
+		        }
+		        return  board;
+		    }
+
 		}
-
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-		TicTacToe obj = new TicTacToe();
-		obj.Board();
-		System.out.println("*********adding the Tic Tac Toe Game on Board***********");
-	}
-
-}
