6105363f944fe55873e43bbb3d91d53477e8ac89
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-28 08:55:22 +0530

    [sushmitha] changed the code for showboard method

--- src/com/bridzelab/TicTacToe/TicTacToe.java
+++ src/com/bridzelab/TicTacToe/TicTacToe.java
@@ -5,11 +5,12 @@ public class TicTacToe
 	
 	public static char UserLetter = ' ';
 	public static char ComputerLetter = ' ';
-
+	static char[] board;
+	 
 
 	public static void main(String[] args) {
 	       System.out.println("************Welcome to TicTacToe on workshop platform*******************\"");
-	       char[] board = createBoard();
+	        board = createBoard();
 	       char UserLetter = chooseLetter();
 	       char ComputerLetter;
 	       if(UserLetter=='X')
@@ -22,24 +23,13 @@ public class TicTacToe
 
 	       System.out.println("Computer Letter is::::::::::: " + ComputerLetter);
 	       System.out.println("User Letter is:::::::::" + UserLetter);
-
+	       showBoard();
 	       
-	       showBoard(board);
 	   }
 
 	    
-	    public static void showBoard(char[] board)
-	    {
-	        System.out.println("  |  " + board[1] + "  |  "  + board[2] + "  |  " + board[3]);
-	        System.out.println("--------------");
-	        System.out.println("  |  " + board[4] + "  |  "  + board[5] + "  |  " + board[6]);
-	        System.out.println("--------------");
-	        System.out.println("  |  " + board[7] + "  |  "  + board[8] + "  |  " + board[9]);
-	        System.out.println("--------------");
-	    }
 
-
-	    public static char[] createBoard() {
+		public static char[] createBoard() {
 
 	        char[] board = new char[10];
 	        for(int index = 0; index < board.length; index++)
@@ -55,5 +45,16 @@ public class TicTacToe
 	        symbol=UserInput.next().toUpperCase().charAt(0);
 	        return symbol;
 	    }
+	    static void showBoard()
+	    {
+			System.out.println("/---|---|---|----\\");
+			System.out.println("| " + board[1] + " 1 " + board[2] + " 2 " + board[3] + " 3"  +   "    |   "  );
+			System.out.println("------------------");
+			System.out.println("| " + board[4] + " 4 " + board[5] + " 5 " + board[6] + " 6"  +   "    |   ");
+			System.out.println("------------------");
+			System.out.println("| " + board[7] + " 7 " + board[8] + " 8 " + board[9] + " 9"  +   "    |   ");
+			System.out.println("/---|---|---|----\\");
+		}
 
+	   
 	}
